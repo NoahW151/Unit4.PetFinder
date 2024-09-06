03b8ab3941bb8993af3f68b07727f432c7a11c0c
NoahW151 <73507568+NoahW151@users.noreply.github.com>
2024-09-06 02:49:55 -0600

    Finished assignment

--- index.js
+++ index.js
@@ -10,8 +10,16 @@ const PORT = 8080;
 // GET - / - returns homepage
 app.get('/', (req, res) => {
     // serve up the public folder as static index.html file
-
+    res.sendFile("./public/index.html", {root: "."});
 });
+
+app.get('/script.js', (req, res) => {
+    res.sendFile("./public/script.js", {root: "."});
+})
+
+app.get('/styles.css', (req, res) => {
+    res.sendFile("./public/styles.css", {root: "."});
+})
 
 // hello world route
 app.get('/api', (req, res) => {
@@ -21,16 +29,19 @@ app.get('/api', (req, res) => {
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
     // send the pets array as a response
+    res.send(pets);
 
 });
 
 // get pet by owner with query string
 app.get('/api/v1/pets/owner', (req, res) => {
     // get the owner from the request
+    const owner = req.query.owner;
 
 
     // find the pet in the pets array
-    const pet = pets.find(pet => pet.owner === owner);
+    const pet = pets.find(pet => pet.owner.toLowerCase() === owner.toLowerCase());
+    res.send(pet)
 
     // send the pet as a response
 
@@ -39,15 +50,18 @@ app.get('/api/v1/pets/owner', (req, res) => {
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
     // get the name from the request
-
+    const name = req.params.name;
 
     // find the pet in the pets array
-    const pet = pets.find(pet => pet.name === name);
+    const pet = pets.find(pet => pet.name.toLowerCase() === name.toLowerCase());
 
     // send the pet as a response
 
+    res.send(pet);
+
 });
 
+// listens on port 8080
 app.listen(PORT, () => {
     console.log('Server is listening on port ' + PORT);
 });
